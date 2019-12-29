67650d40073c680e65ddb9076780bc9dc4f88aa0
nkudinov <oracle_developer@mail.ru>
2019-12-29 17:57:37 +0300

    add FibonacciRDD

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,6 +4,7 @@ import org.apache.spark.sql.types.{LongType, StringType, StructField, StructType
 
 object Main extends App{
   val spark = SparkSession.builder.config("spark.sql.warehouse.dir", "file:///c:/tmp/spark-warehouse").config("spark.sql.codegen.wholeStage", "false").appName("MainApplication Demo").master("local[5]").getOrCreate
+  import spark.implicits._
 
   val schema = new StructType(Array( StructField("id",LongType) , StructField("name", StringType)))
 
@@ -12,8 +13,6 @@ object Main extends App{
        .schema(schema)
        .load()
        .show()
-
-  import spark.implicits._
 
   val fdf = spark.sqlContext.createDataset(new FibonacciRDD(spark.sparkContext,10))
   fdf.show()
